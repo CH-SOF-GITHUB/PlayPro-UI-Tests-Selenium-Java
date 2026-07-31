e515a2277984f030c108fb7661abc7182ccbb8e2
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-07-31 18:12:02 +0100

    add first booking test - instance chakerqa

---
+++ src/test/java/Cypress/cypress/e2e/play/pro/v3/booking/booking1_test_spec.cy.js
@@ -0,0 +1,29 @@
+describe("Booking Suite 1", () => {
+    // Add Hook Cypress
+    beforeEach(() => {
+        cy.visit("https://chakerqa.playpro.fr/connexion");
+        cy.wait(1000);
+        cy.contains('button', 'Accepter').eq(0).click();
+        cy.wait(1000);
+        cy.get('input[name="email"]').eq(0).type("chakerqa-client@yopmail.com");
+        cy.wait(1000);
+        cy.get('input[name="password"]').eq(0).type("Admin1234!");
+        cy.wait(1000);
+        cy.contains('button', 'Me connecter').click();
+        cy.wait(1000);
+        cy.contains('a', 'Réserver').eq(0).click();
+    });
+
+    it("Booking Activity 'Paintball Experience Appointment' ", () => {
+        cy.get('div[role="button"]').contains('h3', 'Paintball Experience Appointment').click();
+        cy.wait(5000);
+        cy.get('div[style*="plus.svg"]').closest('button').click();
+        cy.get('button#menu-button').eq(0).click();
+        cy.wait(1000);
+        cy.contains('button', '30 min').eq(0).click();
+        cy.wait(1000);
+        cy.contains('button', 'Continuer ➔').click();
+        cy.wait(1000);
+        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '09:00').should('exist');
+    });
+});
