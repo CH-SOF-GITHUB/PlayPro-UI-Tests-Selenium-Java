900c845ddf5d8fa3e0550b5faf0705493f1569b9
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-08-05 18:26:55 +0100

    test(booking): add E2E tests for booking 'Exp Rum 2 booking' navigation and reservation workflow (step1)

---
+++ src/test/java/Cypress/cypress/e2e/play/pro/v3/booking/activites/booking2_test_spec.cy.js
@@ -0,0 +1,72 @@
+describe("Booking Suite 2", () => {
+    // Add Hook Cypress
+    beforeEach(() => {
+        cy.visit("https://chakerqa.playpro.fr/connexion");
+        cy.wait(1000);
+        cy.contains('button', 'Accepter').eq(0).click();
+        cy.wait(1000);
+        cy.get('input[name="email"]').eq(0).type("chakerqa-client@yopmail.com");
+        cy.wait(1000);
+        cy.get('input[name="password"]').eq(0).type("Admin1234!");
+        cy.wait(1000);
+        cy.contains('button', 'Me connecter').click();
+        cy.wait(1000);
+        cy.contains('a', 'Réserver').eq(0).click();
+        cy.wait(5000);
+    });
+
+    // TEST 1
+    it("Navigate to Activity tagged by category 'Aventures Immersives' before booking ", () => {
+        cy.contains('h3', 'Aventures Immersives').should('exist');
+        cy.wait(1000);
+        cy.contains('h3', 'Aventures Immersives').click();
+        cy.wait(5000);
+        cy.url().should('equal', 'https://chakerqa.playpro.fr/discover/tag/aventures-immersives?id=4');
+    });
+
+    // TEST 2
+    it("Navigate to Activity for booking ", () => {
+        cy.contains('h3', 'Aventures Immersives').click();
+        cy.wait(5000);
+        cy.contains('h3', 'Exp Rum 2 booking').should('exist');
+        cy.wait(1000);
+        cy.contains('h3', 'Exp Rum 2 booking').click();
+        cy.wait(5000);
+        cy.url().should('equal', 'https://chakerqa.playpro.fr/discover/reservation/exp-rum-2-booking');
+    });
+
+    // TEST 3
+    it("Booking the activity 'Exp Rum 2 booking' - STEP 1.0", () => {
+        cy.contains('h3', 'Aventures Immersives').click();
+        cy.wait(5000);
+        cy.contains('h3', 'Exp Rum 2 booking').click();
+        cy.wait(5000);
+        for (let i = 0; i < 2; i++) {
+            cy.wait(1000);
+            cy.contains('label', "Je souhaite réserver tout l'espace").click();
+        }
+        cy.wait(1000);
+        cy.get('button[id="menu-button"]').click();
+    });
+
+    // TEST 4
+    it("Booking the activity 'Exp Rum 2 booking' - STEP 1.1", () => {
+        cy.contains('h3', 'Aventures Immersives').click();
+        cy.wait(5000);
+        cy.contains('h3', 'Exp Rum 2 booking').click();
+        cy.wait(5000);
+        for (let i = 0; i < 2; i++) {
+            cy.wait(1000);
+            cy.contains('label', "Je souhaite réserver tout l'espace").click();
+        }
+        cy.wait(1000);
+        cy.get('button[id="menu-button"]').click();
+        cy.wait(1000);
+        cy.contains('button', 'Avancé').click();
+        cy.wait(1000);
+        cy.contains('button', 'Continuer ➔').click();
+        cy.wait(3000);
+        cy.contains('span', '10 pers.').should('be.visible');
+        cy.contains('span', '10 pers.').should('exist');
+    });
+});
