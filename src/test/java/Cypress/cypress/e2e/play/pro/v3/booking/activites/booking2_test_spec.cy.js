4f24501edda2a9f58ca499940bdea24da45c9ff2
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-08-07 19:45:44 +0100

    test(cypress): add e2e booking suite covering complete reservation workflow

--- src/test/java/Cypress/cypress/e2e/play/pro/v3/booking/activites/booking2_test_spec.cy.js
+++ src/test/java/Cypress/cypress/e2e/play/pro/v3/booking/activites/booking2_test_spec.cy.js
@@ -1,3 +1,4 @@
+require('@cypress/xpath')
 describe("Booking Suite 2", () => {
     // Add Hook Cypress
     beforeEach(() => {
@@ -68,5 +69,54 @@ describe("Booking Suite 2", () => {
         cy.wait(3000);
         cy.contains('span', '10 pers.').should('be.visible');
         cy.contains('span', '10 pers.').should('exist');
+    });
+
+    // TEST 5
+    it("Booking the activity 'Exp Rum 2 booking' - STEP 2.0", () => {
+        cy.contains('h3', 'Aventures Immersives').click();
+        cy.wait(5000);
+        cy.contains('h3', 'Exp Rum 2 booking').click();
+        cy.wait(1000);
+        cy.get('button[id="menu-button"]').click();
+        cy.wait(1000);
+        cy.contains('button', 'Avancé').click();
+        cy.wait(1000);
+        cy.contains('button', 'Continuer ➔').click();
+        cy.wait(1000);
+        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '09:35').click();
+        cy.wait(1000);
+        cy.xpath('(//button[contains(@class,"relative w-full h-[50px] rounded-md max-md:px-[15px] lg:text-base gap-[10px] opacity-100 font-poppins font-medium text-[15px] leading-[21px] tracking-[0px] text-center border bg-primary")])[1]').click();
+        cy.wait(1000);
+        cy.contains('h2', 'Pour mieux préparer votre venue').should('be.visible');
+        cy.contains('h2', 'Pour mieux préparer votre venue').should('exist');
+    });
+
+    // TEST 6
+    it("Booking the activity 'Exp Rum 2 booking' - STEP 2.1", () => {
+        cy.contains('h3', 'Aventures Immersives').click();
+        cy.wait(5000);
+        cy.contains('h3', 'Exp Rum 2 booking').click();
+        cy.wait(1000);
+        cy.get('button[id="menu-button"]').click();
+        cy.wait(1000);
+        cy.contains('button', 'Avancé').click();
+        cy.wait(1000);
+        cy.contains('button', 'Continuer ➔').click();
+        cy.wait(1000);
+        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '09:35').click();
+        cy.wait(1000);
+        cy.xpath('(//button[contains(@class,"relative w-full h-[50px] rounded-md max-md:px-[15px] lg:text-base gap-[10px] opacity-100 font-poppins font-medium text-[15px] leading-[21px] tracking-[0px] text-center border bg-primary")])[1]').click();
+        cy.wait(1000);
+        cy.contains('button', 'Finaliser').click();
+        cy.wait(5000);
+        cy.url().should('equal', 'https://chakerqa.playpro.fr/Panier');
+        cy.wait(1000);
+        cy.contains('div.relative', '****-4242').should('be.visible');
+        cy.contains('div.relative', '****-4242').click();
+        cy.wait(1000);
+        cy.contains('button', 'Payer maintenant').click();
+        cy.wait(5000);
+        cy.contains('p', 'Merci pour votre réservation ! 🎉').should('be.visible');
+        cy.contains('span', 'Votre réservation a bien été enregistrée.').should('exist');
     });
 });
