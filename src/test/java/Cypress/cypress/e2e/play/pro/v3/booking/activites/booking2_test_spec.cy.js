c03c14ca34504ea785d8b36dc5ac7581c22ccdd7
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-08-07 20:03:43 +0100

    test(cypress): add e2e booking suite covering complete reservation workflow

--- src/test/java/Cypress/cypress/e2e/play/pro/v3/booking/activites/booking2_test_spec.cy.js
+++ src/test/java/Cypress/cypress/e2e/play/pro/v3/booking/activites/booking2_test_spec.cy.js
@@ -103,10 +103,11 @@ describe("Booking Suite 2", () => {
         cy.wait(1000);
         cy.contains('button', 'Continuer ➔').click();
         cy.wait(1000);
-        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '09:35').click();
-        cy.wait(3000);
+        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '10:45').click();
+        cy.wait(1000);
+        // add a condition to be the button enabled before clicking on it
         cy.xpath('(//button[contains(@class,"relative w-full h-[50px] rounded-md max-md:px-[15px] lg:text-base gap-[10px] opacity-100 font-poppins font-medium text-[15px] leading-[21px] tracking-[0px] text-center border bg-primary")])[1]').should('not.have.attr', 'disabled');
-        cy.wait(3000);
+        cy.wait(1000);
         cy.xpath('(//button[contains(@class,"relative w-full h-[50px] rounded-md max-md:px-[15px] lg:text-base gap-[10px] opacity-100 font-poppins font-medium text-[15px] leading-[21px] tracking-[0px] text-center border bg-primary")])[1]').click();
         cy.wait(1000);
         cy.contains('button', 'Finaliser').click();
