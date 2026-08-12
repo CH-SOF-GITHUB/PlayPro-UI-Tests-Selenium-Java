fbf60f319a394dc530bbcee67c66801c6add0f4b
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-08-12 19:44:47 +0100

    run build in jenkins and generate Junit reports

--- src/test/java/Cypress/cypress.config.js
+++ src/test/java/Cypress/cypress.config.js
@@ -3,6 +3,11 @@ const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
     video: true, // generate videos for builds
+    reporter: 'mocha-junit-reporter', // generate junit reports
+    reporterOptions: {
+        mochaFile: 'cypress/results/results-[hash].xml', // output file for junit reports
+        toConsole: true, // output to console
+    },
     e2e: {
         // Désactivez allowCypressEnv si nécessaire : Ajoutez cette option dans votre configuration pour éviter l'avertissement lié à Cypress.env()
         AllowCypressEnv: false,
--- src/test/java/Cypress/cypress/e2e/play/pro/v3/booking/activites/booking2_test_spec.cy.js
+++ src/test/java/Cypress/cypress/e2e/play/pro/v3/booking/activites/booking2_test_spec.cy.js
@@ -83,7 +83,7 @@ describe("Booking Suite 2", () => {
         cy.wait(1000);
         cy.contains('button', 'Continuer ➔').click();
         cy.wait(1000);
-        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '20:25').click();
+        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '21:35').click();
         cy.wait(1000);
         cy.xpath('(//button[contains(@class,"relative w-full h-[50px] rounded-md max-md:px-[15px] lg:text-base gap-[10px] opacity-100 font-poppins font-medium text-[15px] leading-[21px] tracking-[0px] text-center border bg-primary")])[1]').click();
         cy.wait(1000);
@@ -103,7 +103,7 @@ describe("Booking Suite 2", () => {
         cy.wait(1000);
         cy.contains('button', 'Continuer ➔').click();
         cy.wait(1000);
-        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '20:25').click();
+        cy.get('div.relative').find('button[type="button"].w-full').contains('span', '21:35').click();
         cy.wait(1000);
         // add a condition to be the button enabled before clicking on it
         cy.xpath('(//button[contains(@class,"relative w-full h-[50px] rounded-md max-md:px-[15px] lg:text-base gap-[10px] opacity-100 font-poppins font-medium text-[15px] leading-[21px] tracking-[0px] text-center border bg-primary")])[1]').should('not.have.attr', 'disabled');
